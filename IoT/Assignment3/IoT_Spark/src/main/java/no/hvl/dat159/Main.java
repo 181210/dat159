a64c8f3c6341a6e9b2dca5eb4f3ec6097b227367
181210 <181210@stud.hvl.no>
2018-11-22 15:51:56 +0100

    Updated port

--- IoT/Assignment3/IoT_Spark/src/main/java/no/hvl/dat159/Main.java
+++ IoT/Assignment3/IoT_Spark/src/main/java/no/hvl/dat159/Main.java
@@ -1,11 +1,13 @@
 package no.hvl.dat159;
 
 
+
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+
 import static spark.Spark.*;
 
-
+import java.time.LocalDateTime;
 
 public class Main {
 
@@ -15,46 +17,35 @@ public class Main {
 
         temp = new Temperature();
 
+        port(4565);
 
-        port(getHerokuAssignedPort());
 
-
-        get("/tempsensor/current", (req, res) -> {
-
+        get("tempsensor/current", (req, res) -> {
             Gson gson = new Gson();
-
             JsonObject dweet = new JsonObject();
             dweet.addProperty("this", "succeeded");
             dweet.addProperty("by", "getting");
-
+            dweet.addProperty("the", "dweet");
             JsonObject with = new JsonObject();
-            with.addProperty("thing", "temperature_sensor");
-            with.addProperty("content", gson.toJson(temp));
+
+            with.addProperty("created", LocalDateTime.now().withNano(0).toString());
+            with.add("content", gson.toJsonTree(temp));
             dweet.add("with", with);
             res.type("application/json");
-
             return gson.toJson(dweet);
-
-
         });
 
-        //Testing purpose:
-        System.out.println(tempToJson());
-
-
-        put("/tempsensor/current", (req, res) -> {
+        put("tempsensor/current", (req,res) -> {
 
             Gson gson = new Gson();
 
             temp = gson.fromJson(req.body(), Temperature.class);
-
             res.type("application/json");
-
             return gson.toJson(temp);
 
         });
 
-        get("*", ((request, response) -> "404 - not found"));
+        get("*", (request, response) -> "404 - not found");
     }
 
     static String tempToJson () {
@@ -66,12 +57,4 @@ public class Main {
         return jsonInString;
     }
 
-    static int getHerokuAssignedPort() {
-        ProcessBuilder processBuilder = new ProcessBuilder();
-        if (processBuilder.environment().get("PORT") != null) {
-            return Integer.parseInt(processBuilder.environment().get("PORT"));
-        }
-        return 4567; //default port
-    }
 }
-
